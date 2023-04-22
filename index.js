388dd03b8a5ef0c4d3eb51805f5cd7d6ca041ebb
Barun355 <baruntiwary620@gmail.com>
2023-04-22 19:09:31 +0530

    Created server and added the first middleware

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ const PORT = 8080;
 
 const app = express();
 
+// To use the middleware 
+app.use(myMiddleWare)
+
+
+// My custom middleware
+function myMiddleWare (req, res, next){
+    console.log("Inside the middle ware I created");
+    next(); // It will pass on the control to the next step
+}
+
 
 // Starting server
 app.listen(PORT, ()=>{
     console.log("Server started at " + PORT)
-})
+});
